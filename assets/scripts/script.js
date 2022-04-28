ed8beed231cdd50144ae04b30cb156fbda888fa0
sveinnoli <sveinnoli@hotmail.com>
2022-04-28 23:31:59 +0000

    added method to sort imagesw

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,40 @@
 "use strict";
 import { data } from "../data/data.js" // replace with json request later
 
-
-let testList = [1,5,2,3,10,550,9,1,2,5,1,2,91];
-for (let i = 0; i < testList.length; i++) {
-    for (let j = 0; j < testList.length - i; j++) {
-        if (testList[j] > testList[j+1]) {
-            testList[j] += testList[j+1];
-            testList[j+1] = testList[j] - testList[j+1];
-            testList[j]  -= testList[j+1];
-        }
-    }
-}
-console.log(testList);
-
+// Used to keep track of how the current images should be filtered/ordered
 const sortConfig = {
-    "sort" : "descending",
-    "sortBy" : "price",
-
+    "sort" : {
+        "orderBy" : descendingOrder,
+        "sortBy" : "price",
+    },
+    
+    "filter" : {
+        // Here we keep the price range and such
+    }
 }
 
 const gallery = document.querySelector(".gallery");
 
 function startup() {
+    sort(data, sortConfig.sort.orderBy);
     createTemplate(data);
 }
 
-function sort(data) {
+function ascendingOrder(val1, val2) {
+    return val1 < val2;
+}
+
+function descendingOrder(val1, val2) {
+    return val1 > val2;
+}
+
+function sort(data, comparison) {
     // Best case O(n) worst case O(n^2)
+    let sortBy = sortConfig.sort.sortBy;
     let tmp;
     for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data.length - i -1; j++) {
-            if (data[j].image.price > data[j+1].image.price) {
+            if (comparison(data[j].image[sortBy], data[j+1].image[sortBy])) {
                 tmp = data[j+1];
                 data[j+1] = data[j];
                 data[j] = tmp;
@@ -39,7 +42,7 @@ function sort(data) {
         }
     }
 }
-console.log(sort(data));
+
 function createTemplate(data) {
         let fragment = new DocumentFragment();
         fragment.innerHTML = "";
