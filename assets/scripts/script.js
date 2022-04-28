d0436f1cdf91e25460b055206ca0308b7bc5a72d
sveinnoli <sveinnoli@hotmail.com>
2022-04-28 00:15:58 +0000

    added json fetch

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,23 @@
 "use strict";
+const endpoint = "https://api.github.com/gists/ef06e5e0bb9efb83cb388d7260f840fb"
+let data;
+
+// fetch(endpoint).then(results => {
+//     return results.json();
+// }).then(response => {
+//     data = response;
+//     console.log(data);
+//     console.log(JSON.parse(response))
+// }).catch(err => {
+//     console.log(err);
+// })
+
+
 
 const slider = document.getElementById("someslider");
 
+
+// document.appendChild(document.createTextNode(data))
 slider.addEventListener("change", () => {
     console.log(slider.value);
 })
