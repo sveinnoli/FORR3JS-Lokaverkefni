ac43081c013a95fd46cdc67d18b195f2146bb2e1
sveinnoli <46562494+sveinnoli@users.noreply.github.com>
2022-04-28 11:56:37 +0000

    cl

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,7 @@
 "use strict";
-const endpoint = "https://api.github.com/gists/ef06e5e0bb9efb83cb388d7260f840fb"
-let data;
-
-fetch(endpoint).then(results => {
-    return results.json();
-}).then(response => {
-    data = response;
-    console.log(data);
-    console.log(JSON.parse(response))
-}).catch(err => {
-    console.log(err);
-})
-
-
-
 const slider = document.getElementById("someslider");
+const output = document.querySelector("output");
 
-
-// document.appendChild(document.createTextNode(data))
-slider.addEventListener("change", () => {
-    console.log(slider.value);
+slider.addEventListener("input", (e) => {
+    output.innerText = e.target.value;
 })
