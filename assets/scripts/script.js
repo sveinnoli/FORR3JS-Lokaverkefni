7527fe77edab3edf86e2be2190f7e6fa40bbbc3f
sveinnoli <sveinnoli@hotmail.com>
2022-04-28 00:18:06 +0000

    scripts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,15 +2,15 @@
 const endpoint = "https://api.github.com/gists/ef06e5e0bb9efb83cb388d7260f840fb"
 let data;
 
-// fetch(endpoint).then(results => {
-//     return results.json();
-// }).then(response => {
-//     data = response;
-//     console.log(data);
-//     console.log(JSON.parse(response))
-// }).catch(err => {
-//     console.log(err);
-// })
+fetch(endpoint).then(results => {
+    return results.json();
+}).then(response => {
+    data = response;
+    console.log(data);
+    console.log(JSON.parse(response))
+}).catch(err => {
+    console.log(err);
+})
 
 
 
